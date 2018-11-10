d8c97dc2c4f857ebe7286dd49c35243543b9ffc5
Kay Bosompem <kbosompem@enstoa.com>
2018-11-10 14:58:50 -0600

    Adding files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to qfc-bedford
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/qfc_bedford/core_test.clj
@@ -0,0 +1,7 @@
+(ns qfc-bedford.core-test
+  (:require [clojure.test :refer :all]
+            [qfc-bedford.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
