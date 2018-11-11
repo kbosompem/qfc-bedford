a9a73ebad22cf2f0dbe00ddc5c7e187a117a8a9d
Kay Bosompem <kbosompem@enstoa.com>
2018-11-11 07:58:30 -0600

    Adding missed files

--- project.clj
+++ project.clj
@@ -1,6 +1,17 @@
-(defproject qfc-bedford "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+(defproject qfc-bedford "0.1.0"
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [dk.ative/docjure "1.13.0"]
+                 [hiccup "1.0.5"]   
+                 [cheshire "5.8.1"]]
+
+  :plugins [[lein-codox "0.9.6"]
+            [lein-gorilla "0.4.0"]
+            [lein-ancient "0.6.15"]]
+
+  :uberjar-name "qfcbedford.jar"
+  :aot [qfc-bedford.core]
+  :main qfc-bedford.core)
+
+
--- src/qfc_bedford/core.clj
+++ src/qfc_bedford/core.clj
@@ -1,6 +1,71 @@
-(ns qfc-bedford.core)
+(ns qfc-bedford.core
+  (:gen-class)
+  (:require  [cheshire.core :refer :all]
+             [clojure.data.csv :as csv]
+             [clojure.edn :as edn]             
+             [clojure.string :as str]   
+             [dk.ative.docjure.spreadsheet :refer :all]
+             [clojure.java.io :as io]))          
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def midweek-service #{"MIDWEEK SERVICE"})
+(def sunday-service #{" SUNDAY 1ST SERVICE"})
+(def sunday-and-midweek #{"MIDWEEK SERVICE" " SUNDAY 1ST SERVICE"})
+
+(def config (edn/read-string (slurp "configure.edn")))
+(def attkeys (or (:columns config) []))
+(def att-url-root (or (:attendance-url-root config) ""))
+(def att-url-suffix (or (:attendance-url-suffix config) ""))
+
+
+(defn url-encode [s]
+ (java.net.URLEncoder/encode s "UTF-8"))
+
+(defn get-attendance-data [branch]
+  (let [url (str att-url-root (url-encode branch) att-url-suffix)]  
+    (->  url
+         slurp
+         (parse-string keyword))))
+              
+
+(defn get-midweek-services [attendance]
+  (filter #(and
+            (midweek-service (:TYPE_x0020_OF_x0020_SERVICE  %))
+            (< 20170901 (:DATEOFSERVICENUMBER  %))) attendance))
+
+(defn get-sunday-services [attendance]
+  (filter #(and
+            (sunday-service (:TYPE_x0020_OF_x0020_SERVICE  %))
+            (< 20170901 (:DATEOFSERVICENUMBER  %))) attendance))
+
+(defn get-other-services [attendance]
+  (filter #(and
+            (not (sunday-and-midweek (:TYPE_x0020_OF_x0020_SERVICE  %)))
+            (< 20170901 (:DATEOFSERVICENUMBER  %))) attendance))
+
+(defn gen-data 
+  ([coll] (gen-data coll attkeys)) 
+  ([coll keys]
+   (vec (cons (vec (map str keys)) (mapv #(vec (vals (select-keys % keys))) coll)))))
+
+(defn create-spreadsheet [filename coll & args]
+  (let [params (merge {} (apply hash-map args))
+        wb (create-workbook (params :sheetname)
+                            (vec (cons (vec (map str attkeys)) (mapv #(vec (vals (select-keys % attkeys))) coll))))
+        sheet (select-sheet (params :sheetname) wb)
+        header-row (first (row-seq sheet))]
+    (set-row-style! header-row (create-cell-style! wb {:background (params :background),:font (params :font)}))
+    (save-workbook! filename wb)))
+
+(defn gen-workbook! [branch]
+ (let [att-data (get-attendance-data branch)
+       filename (str (str/lower-case (str/replace branch " " "-"))
+                     "-attendance.xlsx")]
+  (save-workbook! filename
+                  (create-workbook 
+                   "midweek" (gen-data (get-midweek-services att-data))
+                   "sundays" (gen-data (get-sunday-services att-data))
+                   "others"  (gen-data (get-other-services att-data))))))
+
+(defn -main [& args]
+ (map gen-workbook! args))
